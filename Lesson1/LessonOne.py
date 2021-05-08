8b5e830b98331f75155040e3d1a5045521634e1c
chkaradimitriou <chkaradimitriou@gmail.com>
2021-05-08 14:29:26 +0300

    init

---
+++ Lesson1/LessonOne.py
@@ -0,0 +1,29 @@
+# 1. Exercise print to console
+# 2. String properties
+# 3. Primitive types
+
+# Strings | Print name length, upper & lower letters.
+x = "name"
+name = "Christos"
+print(len(name))
+print(name.upper())
+print(name.lower())
+name = " Christos "
+print(name.strip())
+
+# String | Print greeting + name + add space
+greeting = "Hello"
+print(greeting + " " + name)
+
+#Int | float
+y = 5
+c = float(y)
+print(y)
+print(c)
+
+# Int | complex
+d = complex(c)
+print(d)
+
+
+
