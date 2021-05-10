73d981438b9ceab643a9873a7af5fe9127ff8eae
chkaradimitriou <chkaradimitriou@gmail.com>
2021-05-10 21:06:06 +0300

    LessonTwo | Final version

--- Lesson2/LessonTwo.py
+++ Lesson2/LessonTwo.py
@@ -1,19 +1,24 @@
 # if else | defterova9mia exiswsh + diakrinousa
 import math
 
-a = 1
-b = 2
-c = -9
+print('Δώστε το Α')
+a = int(input())
+
+print('Δώστε το Β')
+b = int(input())
+
+print('Δώστε το C')
+c = int(input())
 
 d = (b * b) - 4 * a * c
-x1 = -b + math.sqrt(d) / 2 * a
-x2 = -b - math.sqrt(d) / 2 * a
 
 if d > 0:
-    print('Η εξίσωση έχει πάντα δύο ρίζες ' + str(x1))
-    print('Η εξίσωση έχει πάντα δύο ρίζες ' + str(x2))
+    x1 = -b + math.sqrt(d) / 2 * a
+    x2 = -b - math.sqrt(d) / 2 * a
+    print('Η εξίσωση έχει πάντα δύο ρίζες ', "{:.2f}".format(x1))
+    print('Η εξίσωση έχει πάντα δύο ρίζες ', "{:.2f}".format(x2))
 elif d == 0:
     x = - b / 2 * a
-    print('Η εξίσωση έχει μια διπλή πραγματική ρίζα' + str(x))
+    print('Η εξίσωση έχει μια διπλή πραγματική ρίζα' + "{:.2f}".format(x))
 else:
     print('Δεν μπορεί να ληθεί')
