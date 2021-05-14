2906b3ee1e8047586ffd5404e374ff3537a9a5f7
chkaradimitriou <chkaradimitriou@gmail.com>
2021-05-14 18:25:49 +0300

    Dice Challenge

--- DiceChallenge/DiceChallenge.py
+++ DiceChallenge/DiceChallenge.py
@@ -1,14 +1,38 @@
 import random
+from tkinter import *
+
+tk = Tk()
+tk.title("Roll The Dice")
+tk.geometry("650x500")
+
+description = Text(tk, height=1, )
+description.insert(INSERT, 'GAME ON! Click the button to roll the dice.'.center(80))
+description["state"] = DISABLED
+description.pack()
+
+
+def createResultText(result):
+    results = Text(tk, height=1, )
+    results.insert(INSERT, result.center(80))
+    results["state"] = DISABLED
+    results.pack()
 
 # Δημιουργία δύο ζαριών με τυχαίους αριθμούς 1-6 και εκτύπωση αποτελέσματος
+def rollDice():
+    dice1 = random.choice([1, 2, 3, 4, 5, 6])
+    print(dice1)
 
-dice1 = random.choice([1, 2, 3, 4, 5, 6])
-print(dice1)
+    dice2 = random.choice([1, 2, 3, 4, 5, 6])
+    print(dice2)
 
-dice2 = random.choice([1, 2, 3, 4, 5, 6])
-print(dice2)
+    if dice1 == dice2:
+        result = 'Διπλές! Πρώτο Ζάρι : {0}, Δεύτερο Ζάρι : {1}'.format(dice1, dice2)
+        createResultText(result)
+    else:
+        result = 'Έφερες τα εξής : Πρώτο Ζάρι : {0}, Δεύτερο Ζάρι : {1}'.format(dice1, dice2)
+        createResultText(result)
 
-if dice1 == dice2:
-    print("Έφερες διπλές " + str(dice1) + " " + str(dice2))
-else:
-    print("Το πρώτο ζάρι έφερε " + str(dice1) + " και το δεύτερο " + str(dice2))
+button = Button(tk, text="Roll the dice", command=rollDice)
+button.pack()
+
+mainloop()
